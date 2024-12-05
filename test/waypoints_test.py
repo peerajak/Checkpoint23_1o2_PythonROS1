c70a46e91003fd502b3c517b75f223c5411ee94d
peerajak <peerajak@gmail.com>
2024-12-05 11:53:31 +0000

    ready for re-evaluation

--- test/waypoints_test.py
+++ test/waypoints_test.py
@@ -7,7 +7,9 @@ import rosunit
 import unittest
 import rostest
 import sys
+from nav_msgs.msg import Odometry
 import actionlib
+from tf import transformations
 #import random
 import math
 from time import sleep
@@ -15,7 +17,7 @@ PKG = 'tortoisebot_waypoints'
 NAME = 'tortoisebot_waypoints_test'
 
 waypoints = [
-{ 'x' : 0.7, 'y':-0.48},                                #1
+{ 'x' : 2.7, 'y':-0.48},                                #1
 { 'x' : 0.680851835461391, 'y':0.5046365559674899},     #2 
 { 'x' : 0.21495551839700774, 'y':0.5368169079826496},   #3
 { 'x' : 0.23105951276897543, 'y':0.04812895919547926},  #4
@@ -31,15 +33,29 @@ class TestTortoisebotWaypoints(unittest.TestCase):
 
     tolerance_pos = 0.1
     tolerance_yaw = 0.5
-    test_sequence = [3,4,5]#[1,5,2] #[1,5,2,5,8,5,9]
-
+    # test passed test sequence 
+    #test_sequence = [7,8,9]
+    # Error test_sqeunce
+    #test_sequence = [3,4,5]#[1,5,2] #[1,5,2,5,8,5,9]
     def setUp(self):
-        self.rc = WaypointActionClass()
+        self._sub_odom = rospy.Subscriber('/odom', Odometry, self._clbk_odom)
+
+    def _clbk_odom(self, msg):
+        # position
+        self._position = msg.pose.pose.position
+
+        # yaw
+        quaternion = (
+            msg.pose.pose.orientation.x,
+            msg.pose.pose.orientation.y,
+            msg.pose.pose.orientation.z,
+            msg.pose.pose.orientation.w)
+        euler = transformations.euler_from_quaternion(quaternion)
+        self._yaw = euler[2]
+
 
     def done_cb(self,status, result):
-        rospy.loginfo('done')
-        
-
+        rospy.loginfo('done')        
 
     def feedback_cb(self,msg):
         rospy.loginfo('Feedback received: %s', msg)
@@ -49,31 +65,36 @@ class TestTortoisebotWaypoints(unittest.TestCase):
 
         self.client = actionlib.SimpleActionClient('tortoisebot_as', WaypointActionAction)
         self.client.wait_for_server()
-        goal = WaypointActionGoal()
 
-        for waypoint_index_plus_one in self.test_sequence:      
-            waypoint_index = waypoint_index_plus_one - 1
-            goal.position.x = waypoints[waypoint_index].get('x')
-            goal.position.y = waypoints[waypoint_index].get('y')
-            self.goal_x = goal.position.x 
-            self.goal_y = goal.position.y 
-            self.start_position = self.rc._position
-            self.start_yaw = self.rc._yaw
-            self.desired_yaw = math.atan2(self.goal_y -self.start_position.y, self.goal_x - self.start_position.x)
-            self.client.send_goal(goal, done_cb = self.done_cb, feedback_cb=self.feedback_cb )
-            self.client.wait_for_result()
-            result = self.client.get_result()
-            # step 3 Check the expected result with assertion
-            # Check that the end position [X,Y] of the robot is the expected one based on the goal sent.
-            # Check that the end rotation [Yaw] of the robot is the expected one based on the goal sent.
-            self.end_position = self.rc._position
-            self.end_yaw = self.rc._yaw
-            rospy.loginfo('goal ({},{}, yaw {})  end position ({},{}, yaw {})'.format(self.goal_x, self.goal_y, self.rc._des_yaw,self.end_position.x,self.end_position.y,self.end_yaw))        
-            self.assertTrue(abs(self.goal_x - self.end_position.x) <= self.tolerance_pos, "Error X Position not in acceptable range")
-            self.assertTrue(abs(self.goal_y - self.end_position.y) <= self.tolerance_pos, "Error Y Position not in acceptable range")       
-            self.assertTrue(abs(self.desired_yaw - self.end_yaw) <= self.tolerance_yaw, "Error Yaw angle not in acceptable range")
-        #self.rc.shutdownhook()
-        rospy.signal_shutdown('finished')
+
+        # for waypoint_index_plus_one in self.test_sequence: 
+        waypoint_index_plus_one = 9  # 9 is for passed ,# 1 for failed
+        waypoint_index = waypoint_index_plus_one - 1
+        goal = WaypointActionGoal()
+        goal.position.x = waypoints[waypoint_index].get('x')
+        goal.position.y = waypoints[waypoint_index].get('y')
+        self.goal_x = goal.position.x 
+        self.goal_y = goal.position.y 
+        self.start_position = self._position
+        self.start_yaw = self._yaw 
+        self.desired_yaw = math.atan2(self.goal_y -self.start_position.y, self.goal_x - self.start_position.x)
+        self.client.send_goal(goal, done_cb = self.done_cb, feedback_cb=self.feedback_cb )
+        #self.client.send_goal_and_wait(goal)
+        self.client.wait_for_result() 
+        #print(self.client.get_state)
+        result = self.client.get_result()
+        print(result)
+        # step 3 Check the expected result with assertion
+        # Check that the end position [X,Y] of the robot is the expected one based on the goal sent.
+        # Check that the end rotation [Yaw] of the robot is the expected one based on the goal sent.
+        self.end_position = self._position
+        self.end_yaw = self._yaw 
+        rospy.loginfo('goal ({},{}, yaw {})  end position ({},{}, yaw {})'.format(self.goal_x, self.goal_y, self.desired_yaw,self.end_position.x,self.end_position.y,self.end_yaw))        
+        self.assertTrue(abs(self.goal_x - self.end_position.x) <= self.tolerance_pos, "Error X Position not in acceptable range")
+        self.assertTrue(abs(self.goal_y - self.end_position.y) <= self.tolerance_pos, "Error Y Position not in acceptable range")       
+        self.assertTrue(abs(self.desired_yaw - self.end_yaw) <= self.tolerance_yaw, "Error Yaw angle not in acceptable range")
+            
+
 
 
 
